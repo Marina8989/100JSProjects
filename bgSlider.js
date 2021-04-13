51cc26e4b20fc4ca4fe576ae474d4ec19d581f70
Marina8989 <marina.mill209@gmail.com>
2021-04-13 13:41:29 -0700

    js projects

--- bgSlider.js
+++ bgSlider.js
@@ -17,10 +17,11 @@ const right = document.querySelector('.right');
 let count = 0;
 
 right.addEventListener('click', () => {
-  
   console.log(count);
-  container.style.background = img[count].pic;
+  container.style.background = 'url('+ img[count].pic +')';
+  container.style.backgroundSize = 'cover';
   count++;
+  console.log(count);
   if(count === img.length) {
       count = 0;
   }
