bd7452470149bc94fb53f92dfce629795d848edd
Marina8989 <marina.mill209@gmail.com>
2021-04-13 22:05:48 -0700

    js project

---
+++ testimonials.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        userName: 'Andy',
+        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+    },
+    {
+        username: 'Bendy',
+        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+    },
+    {
+        userName: 'Gandy',
+        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+    }
+]
+const userName = document.querySelector('.username');
+const about = document.querySelector('.about');
+const btn = document.querySelectorAll('.btn');
