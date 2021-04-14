9f8e9e627c5bead5a1cc83bde8473b93978baf90
Marina8989 <marina.mill209@gmail.com>
2021-04-13 22:17:53 -0700

    js project

--- testimonials.js
+++ testimonials.js
@@ -1,17 +1,40 @@
 const users = [
     {
         userName: 'Andy',
-        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+        about: 'ahgjajaajag consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
     },
     {
-        username: 'Bendy',
-        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+        userName: 'Bendy',
+        about: 'fjajfgafgafgafgafue, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
     },
     {
         userName: 'Gandy',
-        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptates dolore nihil eligendi sapiente quidem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
+        about: 'cjffsiuryewfhsdsdbsdsj cem animi atque, dolorem fuga facere corrupti? Officiis sed, aliquid quam voluptates ratione repellat nihil aperiam.'
     }
 ]
 const userName = document.querySelector('.username');
 const about = document.querySelector('.about');
 const btn = document.querySelectorAll('.btn');
+let count = 0;
+
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.classList.contains('left')) {
+            count--;
+            if(count < 0) {
+                count = users.length-1;
+            }
+            userName.innerText = users[count].userName;
+            about.innerText = users[count].about;
+        }
+
+        if(button.classList.contains('right')) {
+           count++;
+           if(count > users.length-1) {
+              count = 0;
+           }
+           userName.innerText = users[count].userName;
+           about.innerText = users[count].about;
+        }
+    })
+})
