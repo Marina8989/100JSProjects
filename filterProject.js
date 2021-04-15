fb4eb60c9a17ae6724e28b115c7444ae4d6cc6d2
Marina8989 <marina.mill209@gmail.com>
2021-04-14 17:43:38 -0700

    js project

--- filterProject.js
+++ filterProject.js
@@ -1,5 +1,7 @@
 const btn = document.querySelectorAll('.a');
 const items = document.querySelectorAll('.food');
+const input = document.querySelector('input');
+const searchBtn = document.querySelector('.search');
 
 btn.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -19,6 +21,33 @@ btn.forEach(button => {
         })
     })
 })
+
+
+
+function getInput() {
+    searchBtn.addEventListener('click', () => {
+        if(input.value == '') {
+           console.log('please enter item...');
+           input.value = '';
+        }else {
+            items.forEach(food => {
+                if(input.value == 'all') {
+                   food.style.display = 'block';
+                   input.value = '';
+                }else if(food.classList.contains(`${input.value}`)){
+                    food.style.display = 'block';
+                    input.value = '';
+                }
+                else {
+                    food.style.display = 'none';
+                }
+            })
+            
+       }
+    })
+}
+getInput()
+
 
 
 
