bb8333c218e30e26ae5a343a0ad6b1aced724fbe
Marina8989 <marina.mill209@gmail.com>
2021-04-15 21:20:24 -0700

    js project

--- tipForm.js
+++ tipForm.js
@@ -6,17 +6,56 @@ const btn = document.querySelector('.calculate');
 const x = document.querySelector('.x');
 
 
+   if(scores[1].value == 'great') {
+       btn.addEventListener('click', () => {
+          let s = ((+totals.value * 20) / 100) / (+shares.value);
+            x.innerText = `Each of you have to pay: $${s}`;
+            totalPayment.append(x);
+       })
+  }
 
-btn.addEventListener('click', () => {
-
-    if(totals.value == '' || shares.value == '' || scores.value == '') {
-          console.log('please enter info');
-    }
-    if(scores[1].value == 'great') {
-        let s = ((+totals.value * 20) / 100) / (+shares.value);
-         x.innerText = `Each of you have to pay: $${s}`;
-         totalPayment.append(x);
-    }
-})
+ if(scores[2].value == 'good') {
+     btn.addEventListener('click', () => {
+        let n = ((+totals.value * 10) / 100) / (+shares.value);
+        x.innerText = `Each of you have to pay: $${n}`;
+        totalPayment.append(x);
+     })
+  }
 
 
+
+
+// btn.addEventListener('click', () => {
+//   if(scores[1].value == 'great') {
+//      let s = ((+totals.value * 20) / 100) / (+shares.value);
+//         x.innerText = `Each of you have to pay: $${s}`;
+//         totalPayment.append(x);
+//   }
+//   if(scores[2].value == 'good') {
+//      let n = ((+totals.value * 10) / 100) / (+shares.value);
+//         x.innerText = `Each of you have to pay: $${n}`;
+//         totalPayment.append(x);
+//   }
+
+// })
+
+
+
+
+// let s = ((+totals.value * 20) / 100) / (+shares.value);
+//                   x.innerText = `Each of you have to pay: $${s}`;
+//                   totalPayment.append(x);
+//                   break;
+
+
+
+// let n = ((+totals.value * 10) / 100) / (+shares.value);
+//                  x.innerText = `Each of you have to pay: $${n}`;
+//                  totalPayment.append(x);
+//                  break;
+
+
+// let y = ((+totals.value * 2) / 100) / (+shares.value);
+//                  x.innerText = `Each of you have to pay: $${y}`;
+//                  totalPayment.append(x);
+//                  break; 
