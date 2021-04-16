7e1138a45acb3d32a180dc589ab86c903dcdeffe
Marina8989 <marina.mill209@gmail.com>
2021-04-15 19:48:23 -0700

    js projects

---
+++ tipForm.js
@@ -0,0 +1,4 @@
+const totals = document.querySelector('#totals');
+const shares = document.querySelector('#shares');
+const scores = document.querySelector('#scores');
+const btn = document.querySelector('.calculate');
